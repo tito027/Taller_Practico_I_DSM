425a311ec5482e77976702508259b2e2fd2d3d01
Tito <cruzt6869@gmail.com>
2023-02-25 19:48:40 -0600

    Creando el fragment (vista y funcionalidad) que calcula promedios

--- app/src/main/java/com/example/tallerpracticoi_dsm/AverageScoreFragment.kt
+++ app/src/main/java/com/example/tallerpracticoi_dsm/AverageScoreFragment.kt
@@ -5,6 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import kotlin.math.roundToInt
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +26,30 @@ class AverageScoreFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    // Declarando variables
+    private lateinit var txtName: EditText
+    private lateinit var txtRating1: EditText
+    private lateinit var txtRating2: EditText
+    private lateinit var txtRating3: EditText
+    private lateinit var txtRating4: EditText
+    private lateinit var txtRating5: EditText
+    private lateinit var lblStatus: TextView
+    private lateinit var lblAverage: TextView
+    private lateinit var btnClear: Button
+    private lateinit var btnCalculate: Button
+
+    private fun clear(onlyLabel: Boolean = false) {
+        if(!onlyLabel) {
+            txtName.setText("")
+            txtRating1.setText("")
+            txtRating2.setText("")
+            txtRating3.setText("")
+            txtRating4.setText("")
+            txtRating5.setText("")
+        }
+        lblAverage.text = ""
+        lblStatus.text = ""
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -35,6 +64,75 @@ class AverageScoreFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_average_score, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Inicializando variables
+        txtName = requireView().findViewById(R.id.txtName)
+        txtRating1 = requireView().findViewById(R.id.txtRating1)
+        txtRating2 = requireView().findViewById(R.id.txtRating2)
+        txtRating3 = requireView().findViewById(R.id.txtRating3)
+        txtRating4 = requireView().findViewById(R.id.txtRating4)
+        txtRating5 = requireView().findViewById(R.id.txtRating5)
+        lblStatus = requireView().findViewById(R.id.lblStatus)
+        lblAverage = requireView().findViewById(R.id.lblAverage)
+        btnClear = requireView().findViewById(R.id.btnClear)
+        btnCalculate = requireView().findViewById(R.id.btnCalculate)
+
+        clear(true)
+
+        // Acción a ejecutar al presionar el botón limpiar
+        btnClear.setOnClickListener {
+            clear()
+        }
+
+        // Acción a ejecutar al presionar el botón calcular
+        btnCalculate.setOnClickListener{
+            clear(true)
+
+            val name: String = txtName.getText().toString()
+            val rating1: Double? = txtRating1.getText().toString().toDoubleOrNull()
+            val rating2: Double? = txtRating2.getText().toString().toDoubleOrNull()
+            val rating3: Double? = txtRating3.getText().toString().toDoubleOrNull()
+            val rating4: Double? = txtRating4.getText().toString().toDoubleOrNull()
+            val rating5: Double? = txtRating5.getText().toString().toDoubleOrNull()
+
+            if(
+                name.isEmpty() ||
+                rating1 == null ||
+                rating2 == null ||
+                rating3 == null ||
+                rating4 == null ||
+                rating5 == null
+            ) {
+                Toast.makeText(activity, "Debes indicar todos los valores!!!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if(
+                rating1 < 0 || rating1 > 10 ||
+                rating2 < 0 || rating2 > 10 ||
+                rating3 < 0 || rating3 > 10 ||
+                rating4 < 0 || rating4 > 10 ||
+                rating5 < 0 || rating5 > 10
+            ) {
+                Toast.makeText(activity, "Las notas deben estar entre 0 y 10!!!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            var average = (rating1 + rating2 + rating3 + rating4 + rating5) / 5 // Calculando promedio
+            average = (average * 100.0).roundToInt() / 100.0 // Redondeandolo a 2 decimales
+            if (average >= 6) {
+                lblStatus.setTextColor(getResources().getColor(R.color.teal_700))
+                lblStatus.text = "$name has aprovado!!!"
+            } else {
+                lblStatus.setTextColor(getResources().getColor(androidx.appcompat.R.color.error_color_material_dark))
+                lblStatus.text = "$name has reprobado!!!"
+            }
+            lblAverage.text = "Promedio: $average"
+        }
     }
 
     companion object {
